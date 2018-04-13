1a2b500faa9f64aace5b0dd698e3b73afea5e7d7
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-04-13 09:29:24 +0200

    view path is modified

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -10,6 +10,6 @@ class ContentController extends Controller
 {
     public function sayHello(Twig $twig):string
     {
-        return $twig->render('HelloWorld::content.hello');
+        return $twig->render('HelloWorld::hello');
     }
 }
