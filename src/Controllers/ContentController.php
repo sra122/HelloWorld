221c2024fe547f69aeee95ac17fd769cfdbd67c1
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-04-13 10:26:27 +0200

    topitems

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -8,8 +8,49 @@ use Plenty\Plugin\Templates\Twig;
 
 class ContentController extends Controller
 {
-    public function sayHello(Twig $twig):string
+    public function showTopItems(Twig $twig, ItemDataLayerRepositoryContract $itemRepository):string
     {
-        return $twig->render('HelloWorld::hello');
+        $itemColumns = [
+            'itemDescription' => [
+                'name1',
+                'description'
+            ],
+            'variationBase' => [
+                'id'
+            ],
+            'variationRetailPrice' => [
+                'price'
+            ],
+            'variationImageList' => [
+                'path',
+                'cleanImageName'
+            ]
+        ];
+
+        $itemFilter = [
+            'itemBase.isStoreSpecial' => [
+                'shopAction' => [3]
+            ]
+        ];
+
+        $itemParams = [
+            'language' => 'en'
+        ];
+
+        $resultItems = $itemRepository
+            ->search($itemColumns, $itemFilter, $itemParams);
+
+        $items = array();
+        foreach ($resultItems as $item)
+        {
+            $items[] = $item;
+        }
+        $templateData = array(
+            'resultCount' => $resultItems->count(),
+            'currentItems' => $items
+        );
+
+        return $twig->render('TopItems::hello', $templateData);
+
     }
 }
