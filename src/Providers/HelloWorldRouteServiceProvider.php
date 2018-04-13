05bb88cac6a4b05565cb999e49228b7306bcaf33
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-04-13 11:58:05 +0200

    route is created

---
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace HelloWorld\Providers;
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+class HelloWorldRouteServiceProvider extends RouteServiceProvider
+{
+    public function map(Router $router)
+    {
+        $router->get('hello', 'HelloWorld\CHelloWorld\Containers\HelloWorldContainer@call');
+    }
+}
