05aea184c7e15ed588bcbf0113153228f27b7149
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-05-24 14:39:55 +0200

    changes in debugging the image

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -61,7 +61,6 @@ class ContentController extends Controller
             'currentItems' => $items,
             'imageDetails' => $images
         );
-        print_r($images);
 
         return $twig->render('HelloWorld::content.TopItems', $templateData);
     }
