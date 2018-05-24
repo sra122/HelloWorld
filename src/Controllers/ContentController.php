b8239b3c79cb8870aeda7e1bfaae9b8a32e5a990
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-05-24 13:49:04 +0200

    Extracting items details

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -6,11 +6,54 @@ namespace HelloWorld\Controllers;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
 use Plenty\Modules\Plugin\DataBase\Contracts;
+use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract;
+
 
 class ContentController extends Controller
 {
-    public function sayHello(Twig $twig):string
+    public function sayHello(Twig $twig, ItemDataLayerRepositoryContract $itemRepository):string
     {
-        return $twig->render('HelloWorld::hello');
+        $itemColumns = [
+            'itemDescription' => [
+                'name1',
+                'description'
+            ],
+            'variationBase' => [
+                'id'
+            ],
+            'variationRetailPrice' => [
+                'price'
+            ],
+            'variationImageList' => [
+                'path',
+                'cleanImageName'
+            ]
+        ];
+
+        $itemFilter = [
+            'itemBase.isStoreSpecial' => [
+                'shopAction' => [3]
+            ]
+        ];
+
+        $itemParams = [
+            'language' => 'en'
+        ];
+
+        $resultItems = $itemRepository->search($itemColumns, $itemFilter, $itemParams);
+
+        $items = array();
+
+        foreach($resultItems as $item)
+        {
+            $items[] = $item;
+        }
+
+        $templateData = array(
+            'resultCount' => $resultItems->count(),
+            'currentItems' => $items
+        );
+
+        return $twig->render('HelloWorld::content.TopItems', $templateData);
     }
 }
