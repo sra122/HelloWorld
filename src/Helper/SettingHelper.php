6a981c7b976b876e15c8151288b91ac37e6cd9f8
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-05-28 11:58:38 +0200

    migration is added to order referrer

---
+++ src/Helper/SettingHelper.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace HelloWorld\Helper;
+
+use Plenty\Modules\Plugin\DynamoDb\Contracts\DynamoDbRepositoryContract;
+
+/**
+ * Class SettingsHelper
+ */
+class SettingsHelper
+{
+    const PLUGIN_NAME = 'HelloWorld';
+
+    const TABLE_NAME = 'settings';
+
+    const SETTINGS_ORDER_REFERRER = 'order_referrer';
+
+    /**
+     * @var DynamoDbRepositoryContract
+     */
+    private $dynamoDbRepo;
+
+    /**
+     * @var array
+     */
+    private $settings;
+
+    /**
+     * @param DynamoDbRepositoryContract $dynamoDbRepository
+     */
+    public function __construct(DynamoDbRepositoryContract $dynamoDbRepository)
+    {
+        $this->dynamoDbRepo = $dynamoDbRepository;
+    }
+
+    /**
+     * Save settings to database.
+     *
+     * @param string $name
+     * @param string $value
+     *
+     * @return bool
+     */
+    public function save($name, $value)
+    {
+        return $this->dynamoDbRepo->putItem(self::PLUGIN_NAME, self::TABLE_NAME, [
+            'name'  => [
+                DynamoDbRepositoryContract::FIELD_TYPE_STRING => (string) $name,
+            ],
+            'value' => [
+                DynamoDbRepositoryContract::FIELD_TYPE_STRING => (string) $value,
+            ],
+        ]);
+    }
+
+    /**
+     * Get settings for a given id.
+     *
+     * @param string $name
+     * @param mixed  $default
+     *
+     * @return string|null
+     */
+    public function get($name, $default = null)
+    {
+        $data = $this->dynamoDbRepo->getItem(self::PLUGIN_NAME, self::TABLE_NAME, true, [
+            'name' => [DynamoDbRepositoryContract::FIELD_TYPE_STRING => $name]
+        ]);
+
+        if(isset($data['value'][ DynamoDbRepositoryContract::FIELD_TYPE_STRING ]))
+        {
+            return $data['value'][ DynamoDbRepositoryContract::FIELD_TYPE_STRING ];
+        }
+
+        return $default;
+    }
+}
---
+++ src/Migrations/GetOrderReferrer.php
@@ -0,0 +1,48 @@
+<?php
+namespace HelloWorld\Migrations;
+
+use HelloWorld\Helper\SettingsHelper;
+use Plenty\Modules\Order\Referrer\Contracts\OrderReferrerRepositoryContract;
+use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
+
+/**
+ * Class CreateOrderReferrer
+ */
+class CreateOrderReferrer
+{
+    /**
+     * @var SettingsHelper
+     */
+    private $settingsHelper;
+
+    /**
+     * CreateOrderReferrer constructor.
+     *
+     * @param SettingsHelper $settingsHelper
+     */
+    public function __construct(SettingsHelper $settingsHelper)
+    {
+        $this->settingsHelper = $settingsHelper;
+    }
+
+    /**
+     * @param OrderReferrerRepositoryContract $orderReferrerRepo
+     */
+    public function run(OrderReferrerRepositoryContract $orderReferrerRepo)
+    {
+        $orderReferrer = $orderReferrerRepo->getReferrerById(66.0);
+        $retries = 0;
+
+        do
+        {
+            // due to the fact that CreateSettingsTable migration just run, it could be that DynamoDB needs some time to create the table, so we try again
+            $status = $this->settingsHelper->save(SettingsHelper::SETTINGS_ORDER_REFERRER, $orderReferrer->id);
+
+            if($status === false)
+            {
+                sleep(5);
+            }
+        }
+        while($status === false && ++$retries < 3);
+    }
+}
