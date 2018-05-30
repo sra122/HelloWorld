377ae8984db3692d2e863f8c323cb0d0f8daf988
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-05-30 11:59:47 +0200

    testing the external sdk

--- resources/lib/guzzle_connector.php
+++ resources/lib/guzzle_connector.php
@@ -8,7 +8,7 @@ $res = $client->request(
     'https://jsonplaceholder.typicode.com/posts',
     [
         'headers' => ['Content-type' => 'application/json; charset=UTF-8'],
-        'form_params' => ['title' => SdkRestApi::getParam('title')]
+        'form_params' => ['body' => SdkRestApi::getParam('title')]
     ]
 
 );
