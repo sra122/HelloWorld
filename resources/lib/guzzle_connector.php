ab2d3926f02049a3b18b929e225da08455bc8d6c
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-05-30 10:45:26 +0200

    testing the external sdk with iways template login

--- resources/lib/guzzle_connector.php
+++ resources/lib/guzzle_connector.php
@@ -3,7 +3,7 @@
 $client = new \GuzzleHttp\Client();
 $res = $client->request(
     'POST',
-    'https://www.medion.local/api/product/create.php'
+    'https://templates.i-ways.net/login'
 );
 /** @return array */
 return json_decode($res->getBody(), true);
