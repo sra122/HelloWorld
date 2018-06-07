b238793871d65c45079a281f97f15aca767e6080
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-06-07 10:03:39 +0200

    testing the category by modifying the route

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -13,34 +13,6 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
     {
         $router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
         $router->get('update', 'HelloWorld\Controllers\UpdateItemController@updateItems');
-
-        $api->version(['v1'], ['middleware' => ['oauth']], function ($router) {
-
-            /** Taxonomies */
-            $router->get('markets/etsy/taxonomies', [
-                'uses'       => 'Etsy\Controllers\TaxonomyController@all'
-            ]);
-
-            $router->post('markets/etsy/taxonomies/correlations', [
-                'uses'       => 'Etsy\Controllers\TaxonomyController@saveCorrelations'
-            ]);
-
-            $router->get('markets/etsy/taxonomies/correlations', [
-                'uses'       => 'Etsy\Controllers\TaxonomyController@getCorrelations'
-            ]);
-
-            $router->get('markets/etsy/taxonomies/{id}', [
-                'uses'       => 'Etsy\Controllers\TaxonomyController@get'
-            ]);
-
-            /** Categories */
-            $router->get('markets/etsy/categories', [
-                'uses'       => 'Etsy\Controllers\CategoryController@all'
-            ]);
-
-            $router->get('markets/etsy/categories/{id}', [
-                'uses'       => 'Etsy\Controllers\CategoryController@get'
-            ]);
-        });
+        $router->get('category', 'HelloWorld\Controllers\CategoryList@all');
     }
 }
