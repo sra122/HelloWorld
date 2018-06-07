d3845b5130d9b66908ba157399f4aacbe329d6a2
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-06-07 10:05:21 +0200

    testing the category by modifying the route

--- src/Controllers/CategoryController.php
+++ src/Controllers/CategoryController.php
@@ -64,6 +64,6 @@ class CategoryController extends Controller
             'categoryDetails' => $categoryRepo->all(['lang' => $request->get('lang', 'de')], $with)
         );
 
-        return $twig->render('HelloWorld::content.TopItems', $categoryInfo);
+        return $twig->render('HelloWorld::content.CategoryList', $categoryInfo);
     }
 }
