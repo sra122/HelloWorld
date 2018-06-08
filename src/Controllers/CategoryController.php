20c7a0e2e375b75e29806d9708c5e57d2d5d55b2
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-06-08 14:35:42 +0200

    modifications to the controller error

--- src/Controllers/CategoryController.php
+++ src/Controllers/CategoryController.php
@@ -17,32 +17,6 @@ use Plenty\Plugin\Templates\Twig;
 class CategoryController extends Controller
 {
     /**
-     * Get a category.
-     *
-     * @param Request  $request
-     * @param Response $response
-     * @param int      $id
-     *
-     * @return Response
-     */
-    public function get(Request $request, Response $response, int $id)
-    {
-        $with = $request->get('with', []);
-
-        if (!is_array($with) && strlen($with)) {
-            $with = explode(',', $with);
-        }
-
-        /** @var CategoryRepositoryContract $categoryRepo */
-        $categoryRepo = pluginApp(CategoryRepositoryContract::class);
-
-        $category = $categoryRepo->get($id, $request->get('lang', 'de'), $with);
-
-        return $response->json($category);
-    }
-
-
-    /**
      * Get categories.
      *
      * @param Request $request
