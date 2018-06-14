8b66a1c93ae95b12ab994110256d7e0aa9df3b67
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-06-14 14:54:39 +0200

    extraction of attributes

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -232,7 +232,7 @@ class ContentController extends Controller
 
         //$settingInfo = $settingRepo->get(78);
 
-        $attributes = $attributeMap->all($itemColumns, 50, 1);
+        $attributes = $attributeMap->all([], $perPage = 50, $page = 1);
 
 
         $templateData = array(
