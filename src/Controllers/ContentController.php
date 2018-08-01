d3989ad18d614fc7b2f4872dadca9738fa776b49
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-08-01 10:45:52 +0200

    testing with variationrepository

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -3,7 +3,6 @@ namespace HelloWorld\Controllers;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
 use Plenty\Modules\Plugin\DataBase\Contracts;
-use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract;
 use Plenty\Modules\Item\Variation\Contracts\VariationSearchRepositoryContract;
 use Plenty\Modules\Item\Attribute\Contracts\AttributeRepositoryContract;
 use Plenty\Modules\Item\Property\Contracts\PropertyRepositoryContract;
@@ -22,7 +21,7 @@ use Plenty\Plugin\Http\Request;
 class ContentController extends Controller
 {
     private $parentCategoryArray = [];
-    public function sayHello(Twig $twig, VariationSearchRepositoryContract $itemRepository, VariationRepositoryContract $variationRepo, CategoryRepositoryContract $variationCat, LibraryCallContract $libCall, Request $request, SystemInformationRepositoryContract $sys, WebstoreRepositoryContract $web, SettingsCorrelationFactory $correlation, SettingsRepositoryContract $settingRepo, AttributeRepositoryContract $attributeMap, AuthHelper $oauth):string
+    public function sayHello(Twig $twig, VariationSearchRepositoryContract $itemRepository):string
     {
         $itemColumns = [
             'itemBase' => [
@@ -98,15 +97,6 @@ class ContentController extends Controller
                 'attributeId',
                 'attributeValueId'
             ],
-            /*'itemProperty' => [
-                'itemPropertyId',
-                'propertyId',
-                'propertyValue',
-            ],
-            'variationAttributeValue' => [
-                'attributeId',
-                'attributeValueId'
-            ],*/
             'variationImageList' => [
                 'params' => [
                     'all_images'                                       => [
@@ -161,75 +151,9 @@ class ContentController extends Controller
         $resultItems = $itemRepository->search();
 
         $completeData = $resultItems->getResult();
-        /*$completeData = array();
-        foreach($resultItems as $item)
-        {
-            $multiDim = array();
-            $items = [];
-            $items['variation_data'] = $item;
-            $items['sales_price'] = $variationRepo->show($item->variationBase->id, ['variationSalesPrices' => true], $lang = "de");
-            $childCategory = $variationCat->get($item->variationStandardCategory->categoryId, $lang = "de");
-            $parentCategoryArray = array();
-            $parentCategoryData = '';
-            if($childCategory->parentCategoryId != null) {
-                $parentCategoryData = $variationCat->get($childCategory->parentCategoryId, $lang = "de");
-                if($parentCategoryData->parentCategoryId != null) {
-                    $parentCategoryArray = $this->parentCategory($parentCategoryData, $variationCat);
-                }
-            }
-            $parentCatSet = '';
-            foreach($parentCategoryArray as $key => $parentCategory)
-            {
-                $parentCatSet .= $parentCategory->details[0]->name;
-                if ($key != count($parentCategoryArray) - 1) {
-                    $parentCatSet .= '<<';
-                }
-            }
-            if(empty($parentCatSet)) {
-                $items['categories'] = $parentCategoryData->details[0]->name .' << ' .$childCategory->details[0]->name;
-            } else {
-                $items['categories'] = $parentCatSet .' << ' . $parentCategoryData->details[0]->name .' << ' .$childCategory->details[0]->name;
-            }
-            array_push($multiDim, $items);
-            array_push($completeData, $multiDim);
-        }
-        $systemDetails = $web->findByPlentyId($sys->loadValue('plentyId'));
-        $categories = $variationCat->getLinklistTree('item', $lang = "de", $systemDetails->id);
-        $plentyCategoryRepo = pluginApp(CategoryRepositoryContract::class);
-        //$children = $plentyCategoryRepo->getChildren($plentyCategoryRepo->id, $lang);
-        //$info = $request->get('correlations', []);
-        $correlations = $correlation->type('category')
-            ->all('HelloWorld');
-        $settingInfo = [];
-        foreach ($correlations as $correlation)
-        {
-            $settingDetails = $settingRepo->get((int)$correlation['settingsId']);
-            array_push($settingInfo, $settingDetails->settings);
-        }
-        //$settingInfo = $settingRepo->get(78);
-        $attributeValueRepository = pluginApp(AttributeValueRepositoryContract::class);
-        $authHelper = pluginApp(AuthHelper::class);
-        $attributes = $authHelper->processUnguarded(function () use ($attributeValueRepository) {
-            return $attributeValueRepository->findById(4, 93);
-        });
-        //$attributes = $attributeMap->show(2);*/
         $templateData = array(
             'completeData' => $completeData
         );
-        /*$packagistResult = array(
-            'results' =>   $libCall->call('HelloWorld::guzzle_connector', ['title' => 'Berlin'])
-        );*/
         return $twig->render('HelloWorld::content.TopItems', $templateData);
-    }
-    public function parentCategory($parentCategoryArray, $variationCat)
-    {
-        $parentCategoryData = $variationCat->get($parentCategoryArray->parentCategoryId, $lang = "de");
-        if($parentCategoryArray->parentCategoryId != null) {
-            $parentCategoryInfo[] = $parentCategoryData;
-            $this->parentCategory($parentCategoryArray, $variationCat);
-        } else {
-            $parentCategoryInfo[] = $parentCategoryData;
-            return $parentCategoryInfo;
-        }
     }
 }
