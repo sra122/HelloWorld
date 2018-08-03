5f3e93c3869a0c74b0393a8abf8d1eceb7c4c167
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-08-03 13:29:36 +0200

    modification to Post request

--- src/Controllers/CategoryController.php
+++ src/Controllers/CategoryController.php
@@ -42,9 +42,6 @@ class CategoryController extends Controller
             'categoryDetails' => $categoryRepo->all(['lang' => $request->get('lang', 'de')], $with)
         );
 
-
-        //return $twig->render('HelloWorld::content.CategoryList', $categoryInfo);
-
         return $categoryInfo;
     }
 
