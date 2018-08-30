a9d5f0835059c6337c23a3b006576349c126769c
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-08-30 11:22:14 +0200

    testing getLinklistTree

--- src/Controllers/CategoryController.php
+++ src/Controllers/CategoryController.php
@@ -38,7 +38,7 @@ class CategoryController extends Controller
 
         $categoryRepo = pluginApp(CategoryRepositoryContract::class);
 
-        $categoryInfo = $categoryRepo->search($categoryId = null, 1, 50, $with, ['lang' => $request->get('lang', 'de')]);
+        $categoryInfo = $categoryRepo->getLinklistTree($type = "item", $lang = "de", $clientId = null, $maxLevel = 6);
 
         return $categoryInfo;
     }
