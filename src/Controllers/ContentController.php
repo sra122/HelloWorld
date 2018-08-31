4e08c58f621d5fb5942979ebd70d6fd52622746b
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-08-31 13:52:34 +0200

    displaying only mapping data and items info

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -160,7 +160,7 @@ class ContentController extends Controller
         ]);
 
         $itemRepository->setFilters([
-            'referrerId' => 9.0
+            'referrerId' => 13.0
         ]);
         $resultItems = $itemRepository->search();
 
@@ -188,15 +188,15 @@ class ContentController extends Controller
             );*/
 
 
-            $itemInfo = $authHelper->processUnguarded(
+            /*$itemInfo = $authHelper->processUnguarded(
                 function () use ($imageRepo, $item) {
                     return $imageRepo->findByItemId(132);
                 }
-            );
+            );*/
 
 
-            $item->imageDetails = $itemInfo;
-            array_push($imageData, $itemInfo);
+            //$item->imageDetails = $itemInfo;
+            //array_push($imageData, $itemInfo);
         }
 
         $categoryMapping = $settingsRepositoryContract->search(['marketplaceId' => 'HelloWorld', 'type' => 'category'], 1, 100)->toArray();
@@ -204,7 +204,7 @@ class ContentController extends Controller
 
         $templateData = array(
             'completeData' => $items,
-            'imageInfo' => $imageData
+            'imageInfo' => $categoryMapping
         );
         return $twig->render('HelloWorld::content.TopItems', $templateData);
     }
