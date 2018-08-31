ddcb82d7f7a43853b3b78edbd41b333d00697616
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-08-31 13:08:33 +0200

    image data extraction

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -176,8 +176,8 @@ class ContentController extends Controller
 
             $itemInfo = $authHelper->processUnguarded(
                 function () use ($imageRepo, $item) {
-                    $imageWithId = $imageRepo->findByItemId(132);
-                    return $imageRepo->findByImageId($imageWithId->imageId);
+                    return $imageRepo->findByItemId(132);
+                    //return $imageRepo->findByImageId($imageWithId->imageId);
                 }
             );
 
