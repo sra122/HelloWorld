6dfb231a7cfb59a0f129c1bcc6a1ef8bee836303
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-11-27 16:49:23 +0100

    extracting all the items

--- src/Controllers/ItemsStatusController.php
+++ src/Controllers/ItemsStatusController.php
@@ -39,7 +39,7 @@ class ItemsStatusController extends Controller
         $orderReferrerRepo = pluginApp(OrderReferrerRepositoryContract::class);
         $orderReferrerLists = $orderReferrerRepo->getList(['name', 'id']);
 
-        $pandaBlackReferrerID = [];
+        /*$pandaBlackReferrerID = [];
 
         foreach($orderReferrerLists as $key => $orderReferrerList)
         {
@@ -53,7 +53,7 @@ class ItemsStatusController extends Controller
                 'referrerId' => (int)$pandaBlackId['id'],
                 'isMain' => false
             ]);
-        }
+        }*/
 
 
         $resultItems = $itemRepository->search();
