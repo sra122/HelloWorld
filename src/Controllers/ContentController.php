4a86959627bb2836c5c2f7bc6e802282002215ce
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-02-05 17:21:27 +0100

    modification to return value

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -366,7 +366,7 @@ class ContentController extends Controller
         $crons = $settingsRepositoryContract->search(['marketplaceId' => 'HelloWorld', 'type' => 'property'], 1, 100)->toArray();
 
         foreach($resultItems->getResult() as $key => $variation) {
-            if((time() - strtotime($variation['updatedAt'])) < 3600) {
+            if((time() - strtotime($variation['updatedAt'])) < 3600 && isset($categoryId[$variation['variationCategories'][0]['categoryId']])) {
                 $completeData[$variation['id']] = $variation;
             }
         }
