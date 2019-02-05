020c7143b25dc321da899da000cbde83ee582541
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-02-05 16:52:07 +0100

    modification to the data extraction function

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -302,7 +302,55 @@ class ContentController extends Controller
 
     public function extractItemInfo()
     {
-        return 'test';
+        $itemRepository = pluginApp(VariationSearchRepositoryContract::class);
+
+        $itemRepository->setSearchParams([
+            'with' => [
+                'item' => null,
+                'lang' => 'de',
+                'variationSalesPrices' => true,
+                'variationCategories' => true,
+                'variationClients' => true,
+                'variationAttributeValues' => true,
+                'variationSkus' => true,
+                'variationMarkets' => true,
+                'variationSuppliers' => true,
+                'variationWarehouses' => true,
+                'variationDefaultCategory' => true,
+                'unit' => true,
+                'variationStock' => [
+                    'params' => [
+                        'type' => 'virtual'
+                    ],
+                    'fields' => [
+                        'stockNet'
+                    ]
+                ],
+                'stock' => true,
+                'images' => true,
+            ]
+        ]);
+
+        $orderReferrerRepo = pluginApp(OrderReferrerRepositoryContract::class);
+        $orderReferrerLists = $orderReferrerRepo->getList(['name', 'id']);
+
+        $pandaBlackReferrerID = [];
+
+        foreach($orderReferrerLists as $key => $orderReferrerList)
+        {
+            if(trim($orderReferrerList->name) === 'PandaBlack' && count($pandaBlackReferrerID) === 0) {
+                array_push($pandaBlackReferrerID, $orderReferrerList);
+            }
+        }
+
+        foreach($pandaBlackReferrerID as $pandaBlackId) {
+            $itemRepository->setFilters([
+                'referrerId' => (int)$pandaBlackId['id']
+            ]);
+        }
+
+
+        return $pandaBlackReferrerID;
     }
 
 }
